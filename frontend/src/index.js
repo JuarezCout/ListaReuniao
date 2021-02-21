2218583ee719fa1464682e95c5bdd309d8b2680b
JuarezCout <juarezcout15.jl@gmail.com>
2021-02-21 14:15:14 -0300

    Alteracao de arquivo index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './main/App'
 import registerServiceWorker from './registerServiceWorker'
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
 
 Amplify.configure(config);
 
